07f070593c6fe35d1f753c5da44cb45252c80a8e
Alok Singh <54284060+alokkpsingh123@users.noreply.github.com>
2023-04-26 14:59:17 +0530

    notification-component done

--- src/app/notification/notification.component.ts
+++ src/app/notification/notification.component.ts
@@ -2,16 +2,52 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-notification',
-  template: `<div class = "alert alert-success" [hidden] = "displayNoti">
-    This website uses cookies to provide better user experience.
-    <div class = "close"><button class= "btn" (click)="closeNotification()">X</button></div>
-  </div>`,
-  styles: ["div{margin: 10px 0px; padding: 10px 20x; text-align:center;}", "p{font-size: 14px;}",".close{float: right; margin-top: -15px;}"]
+  template: `
+    <div class="alert alert-success" [hidden]="displayNoti">
+      <p>This website uses cookies to provide better user experience.</p>
+      <button class="btn" (click)="closeNotification()">X</button>
+    </div>
+  `,
+  styles: [
+    `
+      div {
+        display: flex;
+        margin: 10px 0px;
+        padding: 10px 20px;
+        text-align: center;
+        justify-content: center;
+        background-color: #cfe0dd;
+      }
+    `,
+    `
+      p {
+        font-size: 18px;
+        color: #1d6459;
+      }
+    `,
+    `
+      button {
+        margin-left: 30px;
+        align-self: flex-end;
+        padding: 2px 5px;
+        border: none;
+        background-color: #cfe0dd;
+        transition: ease-in-out;
+      }
+    `,
+    `
+      button:hover {
+        background-color: #1d6459;
+        transition: ease-in-out;
+      }
+    `,
+  ],
 })
 export class NotificationComponent {
-  displayNoti:boolean = false;
+  displayNoti: boolean = false;
 
-  closeNotification(){
+  closeNotification() {
     this.displayNoti = true;
+    console.log('Function clicked');
   }
 }
