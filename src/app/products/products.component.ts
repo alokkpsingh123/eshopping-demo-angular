4f97b9169f8266151801e9a59f4b9e7d974788c6
Alok Singh <54284060+alokkpsingh123@users.noreply.github.com>
2023-04-27 11:32:58 +0530

    search-component done

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -11,7 +11,7 @@ export class ProductsComponent {
       {id: 2, name: "Hisense Ultra HD Smart TV" , price:'3339',color:'black', available:'Available', image : '/assets/product2.jpeg'},
       {id: 3, name: "Apple iPhone 12" , price:'1855',color:'black', available:'Not Available', image : '/assets/product3.jpeg'},
       {id: 4, name: "LG Fully Automatic Washing Machine" , price:'1765',color:'white', available:'Available', image : '/assets/product4.jpeg'},
-      {id: 5, name: "LG Refrigerator with Door Cooling" , price:'2815',color:'white', available:'NotAvailable', image : '/assets/product5.jpeg'},
+      {id: 5, name: "LG Refrigerator with Door Cooling" , price:'2815',color:'white', available:'Not Available', image : '/assets/product5.jpeg'},
       {id: 6, name: "DELL Inspiron one 27 Ryzen 7" , price:'2145',color:'white', available:'Available', image : '/assets/product6.jpeg'},
     ];
 }
